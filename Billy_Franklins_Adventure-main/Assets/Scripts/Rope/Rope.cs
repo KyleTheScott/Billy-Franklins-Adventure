36749c47ba17915ba758bce7521489804bd01c30
Neil <neil.hunter78@gmail.com>
2021-04-30 23:32:23 -0400

    Made a basic rope

---
+++ Billy_Franklins_Adventure-main/Assets/Scripts/Rope/Rope.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D hook;
+
+    [SerializeField] GameObject linkPrefab;
+
+    [SerializeField] Weight weight;
+
+    [SerializeField] int links = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateRope();
+    }
+
+    void GenerateRope()
+    {
+        Rigidbody2D previousRB = hook;
+        for (int i = 0; i < links; i++)
+        {
+            GameObject link = Instantiate(linkPrefab, transform);
+            HingeJoint2D joint = link.GetComponent<HingeJoint2D>();
+            joint.connectedBody = previousRB;
+
+            if (i < links - 1)
+            { 
+                previousRB = link.GetComponent<Rigidbody2D>();
+            }
+            else
+            {
+                weight.ConnectRopeEnd(link.GetComponent<Rigidbody2D>());
+            }
+        }
+    }
+}
---
+++ Billy_Franklins_Adventure-main/Assets/Scripts/Rope/Weight.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class Weight : MonoBehaviour
+{
+    [SerializeField] float distanceFromChainEnd = .1f;
+
+    public void ConnectRopeEnd(Rigidbody2D endRB)
+    {
+        HingeJoint2D joint = gameObject.AddComponent<HingeJoint2D>();
+        joint.autoConfigureConnectedAnchor = false;
+        joint.connectedBody = endRB;
+        joint.anchor = Vector2.zero;
+        joint.connectedAnchor = new Vector2(0f, -distanceFromChainEnd);
+
+    }
+}
