437a7f876932fc75f5e584848bc0f7766cb806b2
Neil <neil.hunter78@gmail.com>
2021-05-01 01:25:29 -0400

    Rope & Platform
    Worked on a suspendable platform and attaching the breakable rope

--- Billy_Franklins_Adventure-main/Assets/Scripts/Rope/Weight.cs
+++ Billy_Franklins_Adventure-main/Assets/Scripts/Rope/Weight.cs
@@ -5,6 +5,7 @@ public class Weight : MonoBehaviour
 {
     [SerializeField] float distanceFromChainEnd = .1f;
 
+    //connects the final weight link to the last link
     public void ConnectRopeEnd(Rigidbody2D endRB)
     {
         HingeJoint2D joint = gameObject.AddComponent<HingeJoint2D>();
