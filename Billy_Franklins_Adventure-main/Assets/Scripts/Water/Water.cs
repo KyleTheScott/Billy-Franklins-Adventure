b5e2d56c30e6de2907d26ef02772cfd4a96ccfce
Neil <neil.hunter78@gmail.com>
2021-05-10 11:46:40 -0400

    Worked on metal and water

--- Billy_Franklins_Adventure-main/Assets/Scripts/Water/Water.cs
+++ Billy_Franklins_Adventure-main/Assets/Scripts/Water/Water.cs
@@ -1,9 +1,23 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class Link : MonoBehaviour
+public class Water : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
     public void OnTriggerEnter2D(Collider2D collision)
-    { 
+    {
         //If rope is hit by lightning...
         if (collision.CompareTag("Lightning"))
         {
@@ -11,9 +25,10 @@ public class Link : MonoBehaviour
             // If I cache it then every link will have the platform // might be better
             Transform platform = transform.parent.Find("SuspendedPlatform");
             platform.gameObject.GetComponent<SuspendedPlatform>().SetGrounded();
-            
+
             //destroy rope link
             Destroy(gameObject);
         }
     }
+
 }
