624a6f370091928e00c0ba530c6a3ab3c6e8baef
Flavio Araujo Silva <silvaflavio267@gmail.com>
2021-06-12 14:35:15 -0400

    Set the PuzzleController to ExecutionOrder (-100)

--- Billy_Franklins_Adventure-main/Assets/Scripts/Scenes/PuzzleController.cs
+++ Billy_Franklins_Adventure-main/Assets/Scripts/Scenes/PuzzleController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[DefaultExecutionOrder(-100)]
 public class PuzzleController : MonoBehaviour
 {
     [SerializeField]
