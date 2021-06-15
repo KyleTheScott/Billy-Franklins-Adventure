52002d70e4d4a523380c469ee0aa38c632b2048d
Bryan Gadd <x-dragola-x@live.ca>
2021-06-15 19:54:45 -0400

    Added UI's with Partial Functionality
    - Main Menu scene + UI + script.
    - Settings UI + script.
    - Pause Menu script + UI.

---
+++ Billy_Franklins_Adventure-main/Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    private Canvas mainMenuUI = null;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        try
+        {
+            mainMenuUI = GameObject.Find("Main Menu UI").GetComponent<Canvas>();
+        }
+        catch
+        {
+            Debug.Log("Couldn't get settings UI...");
+        }
+    }
+
+    public void Back()
+    {
+        if (mainMenuUI != null) 
+        {
+            mainMenuUI.gameObject.SetActive(true);
+            mainMenuUI.GetComponent<StartMenu>().SettingsUICalled();
+        }
+    }
+
+    public void StartMenuUICalled()
+    {
+        mainMenuUI.gameObject.SetActive(false);
+    }
+}
