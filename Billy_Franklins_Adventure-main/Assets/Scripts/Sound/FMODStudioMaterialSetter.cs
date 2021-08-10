23922ed9fc0fe459106e684ef7878e251c224ae2
Neil <neil.hunter78@gmail.com>
2021-08-10 12:26:02 -0400

    Added sprite sheets and Player animation states. Also cleaned up folders

---
+++ Billy_Franklins_Adventure-main/Assets/Scripts/Sound/FMODStudioMaterialSetter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODStudioMaterialSetter : MonoBehaviour
+{
+    [SerializeField]
+    private float materialValue;
+    public float MaterialValue => materialValue;
+}
