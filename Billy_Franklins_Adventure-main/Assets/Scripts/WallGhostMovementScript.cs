5b6fc3aee1546710d1661071ce25442eae9f380d
FlavioJorge AraujoSilva <silvaflavio267@gmail.com>
2021-08-13 16:02:57 +0100

    Added in the new ghost wall and ghosts

---
+++ Billy_Franklins_Adventure-main/Assets/Scripts/WallGhostMovementScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallGhostMovementScript : MonoBehaviour
+{
+    [SerializeField]
+    private float RotateSpeed = 5.0f;
+    [SerializeField]
+    private float Radius = 0.15f;
+    private Vector2 centre;
+    private float angle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        centre = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle += RotateSpeed * Time.deltaTime;
+
+        Vector2 offset = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle)) * Radius;
+        transform.position = centre + offset;
+    }
+}
