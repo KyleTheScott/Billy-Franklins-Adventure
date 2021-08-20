dbbc428589cd92e093d6f01ac4b95ffcbf3c6acc
Neil <neil.hunter78@gmail.com>
2021-08-20 16:01:35 -0400

    Fixed player object interaction bugs and lantern lamp bugs

--- Billy_Franklins_Adventure-main/Assets/Scripts/Water/Bucket.cs
+++ Billy_Franklins_Adventure-main/Assets/Scripts/Water/Bucket.cs
@@ -115,6 +115,7 @@ public class Bucket : MonoBehaviour, IInteractable
 
     public void SetHighlighted(bool state)
     {
+        Debug.LogError("highlight bucket: " + state);
         highlight.SetActive(state);
     }
 }
